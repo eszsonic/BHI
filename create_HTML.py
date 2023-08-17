29dfc68d15a1b6943a691d883b9da73efe911dee
eszsonic <esazonov@eng.ua.edu>
2023-08-16 22:32:54 -0500

    generate list

--- create_HTML.py
+++ create_HTML.py
@@ -5,15 +5,16 @@ import os
 import re
 
 
-current_dirname = os.path.dirname(__file__)
+
 
 
 ### Root Directories
 year='2023'
+path_current_dir = os.path.dirname(__file__)
 path_directory_Excel = r'C:\Users\esazo\Desktop\_temp'
 path_directory_Bibtex = r'C:\Users\esazo\Desktop\_temp'
-path_save_directory_bib = os.path.join(current_dirname, r'wwwroot/' + year + r'\bib')
-path_save_directory_html = os.path.join(current_dirname, r'wwwroot/' + year)
+path_save_directory_bib = os.path.join(path_current_dir, r'wwwroot/' + year + r'\bib')
+path_save_directory_html = os.path.join(path_current_dir, r'wwwroot/' + year)
 
 Excel_File_Name = 'ieeebhi2023-papers (1).xlsx'
 Bibtex_File_Name = '30715.bib'
@@ -41,14 +42,18 @@ Paper_Number = df['#']
 Conference_Title = '2023 IEEE EMBS International Conference on Biomedical and HealthInformatics (BHI) (IEEE BHI 2023)'
 
 ### Inputs from the Bibtex
-ArXiv_Link = 'https://www.ieee.org//'
+# ArXiv_Link = 'https://www.ieee.org//'
 pdf_link = 'https://www.ieee.org//'
 # Bib_Address = 'file:///' + 'D:/pdf2Bib/new0.bib'
 
 
+#List of Links to papers to ba later saved as an index file
+linksToPapers=[]
+
+
 ## Create the HTML File
 for i in range(len(df)):
-    if (pd.isna(Registration[i]) == False):
+    if (pd.isna(Registration[i]) is False):
         # try:
             for j in range(len(bib_database.entries)):
                 new_string = bib_database.entries[j]['title'].replace('\n', ' ')
@@ -132,16 +137,19 @@ for i in range(len(df)):
             file_bib.write(BibTex_ID)
             file_bib.close()
 
-            Bib_Address = 'file:///' + path_directory_Bibtex + '/' + str(Paper_Number[i]) + '.bib'
+            #File links
+            # Bib_Address = 'file:///' + path_directory_Bibtex + '/' + str(Paper_Number[i]) + '.bib'
+            Bib_Address = 'bib/' + str(Paper_Number[i]) + '.bib'
+            pdf_link = 'pdfs/' + str(Paper_Number[i]) + '.pdf'
 
             f = open(path_save_directory_html+r'/'+str(Paper_Number[i]) + '.html', 'w', encoding="utf-8")
             ### HTML Template
             html_template = '<!DOCTYPE html>\n' + '<html>\n' + '<head>\n'\
             + '<meta http-equiv="content-type" content="text/html; charset=UTF-8">\n'\
             + '<title>' + str(Paper_Number[i]) + '.html</title>\n' + '</head>\n' + '<body>\n'\
-            + '<div id="papertitle" style="box-sizing: border-box; font-size: 36px; max-width: 750px; overflow-wrap: break-word; white-space: normal; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;">\n'\
+            + '<div id="papertitle" style="box-sizing: border-box; font-size: 36px; max_width: 900px; overflow-wrap: break-word; white-space: normal; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;">\n'\
             + str(Title[i]) +'<dd style="box-sizing: border-box; line-height: 1.42857; margin-left: 0px;"><br>\n' + '</dd>\n' + '</div>\n'\
-            + '<div id="authors" style="box-sizing: border-box; max-width: 750px; overflow-wrap: break-word; white-space: normal; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;"><br\n'\
+            + '<div id="authors" style="box-sizing: border-box; max_width: 900px; overflow-wrap: break-word; white-space: normal; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;"><br\n'\
             + 'style="box-sizing: border-box;">\n'\
             + '<b style="box-sizing: border-box; font-weight: bold;"><i style="box-sizing: border-box;">\n' + str(Authors[i])\
             + '</i></b>; ' + Conference_Title + '</div>'\
@@ -150,9 +158,9 @@ for i in range(len(df)):
             +'style="color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial; display: inline !important; float: none;"></span><br\n'\
             +'style="box-sizing: border-box; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;">\n'\
             +'<br style="box-sizing: border-box; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;">\n'\
-            +'<div id="abstract" style="box-sizing: border-box; max-width: 750px; overflow-wrap: break-word; white-space: normal; text-align: justify; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;">\n'\
+            +'<div id="abstract" style="box-sizing: border-box; max_width: 900px; overflow-wrap: break-word; white-space: normal; text-align: justify; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;">\n'\
             + str(Abstract[i]) + '</div>\n' \
-            + '<div id="keywords" style="box-sizing: border-box; max-width: 750px; overflow-wrap: break-word; white-space: normal; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;"><br\n'\
+            + '<div id="keywords" style="box-sizing: border-box; max_width: 900px; overflow-wrap: break-word; white-space: normal; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;"><br\n'\
             + 'style="box-sizing: border-box;">\n'\
             + '<b style="box-sizing: border-box; font-weight: bold;"><i style="box-sizing: border-box;">\n' + 'Keywords: ' +str(Keywords[i]) + '<br> <br>'\
             +'<font style="box-sizing: border-box; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;"\n'\
@@ -163,42 +171,67 @@ for i in range(len(df)):
             +'<br style="box-sizing: border-box; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;">\n'\
             +'<dd style="box-sizing: border-box; line-height: 1.42857; margin-left: 0px; color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, Verdana, sans-serif; font-size: 16px; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; font-weight: 400; letter-spacing: normal; orphans: 2; text-align: left; text-indent: 0px; text-transform: none; widows: 2; word-spacing: 0px; -webkit-text-stroke-width: 0px; white-space: normal; background-color: rgb(255, 255, 255); text-decoration-thickness: initial; text-decoration-style: initial; text-decoration-color: initial;">[<a\n'\
             +'href="' + pdf_link + '"\n'\
-            +'style="box-sizing: border-box; background-color: transparent; color: rgb(115, 149, 197); text-decoration: none;">pdf</a>]\n'\
+            +'style="box-sizing: border-box; background-color: transparent; color: rgb(115, 149, 197); text-decoration: none;">Full text PDF</a>]\n'\
             +'[<a href="' + Bib_Address +'"\n'\
-            +'style="box-sizing: border-box; background-color: transparent; color: rgb(115, 149, 197); text-decoration: none;">Bibtex</a>]\n'\
-            +'[<a href="' + ArXiv_Link + '" style="box-sizing: border-box;  background-color: transparent; color: rgb(115, 149, 197); text-decoration: none;">arXiv</a>]<span>&nbsp;</span>\n'\
-            +'<div class="bibtex" style="box-sizing: border-box; font-weight: normal; text-decoration: none; display: inline; margin-right: 5px;">'\
-            +'</dd>\n' +'<p></p>\n' +'</body>\n' +  '<span style="font-weight: normal">' + '@'+ bib_database.entries[j]['ENTRYTYPE']+'{'+bib_database.entries[j]['ID']+',\n'\
-            + '<br> author    = {'+paper_author+'},\n'\
-            +' <br> title    = {'+bib_database.entries[j]['title']+'},\n'\
-            +' <br> booktitle    = {'+paper_booktitle+'},\n'\
-            +' <br> address    = {'+paper_address+'},\n'\
-            +' <br> pages    = {'+paper_page+'},\n'\
-            +' <br> days    = {'+paper_days+'},\n'\
-            +' <br> month    = {'+paper_month+'},\n'\
-            +' <br> year    = {'+paper_year+'},\n'\
-            +' <br> keywords    = {'+paper_keywords+'},\n'\
-            +' <br> abstract    = {'+paper_abstract+'}\n'\
-            +' <br> }' + '\n</html>\n'
+            +'style="box-sizing: border-box; background-color: transparent; color: rgb(115, 149, 197); text-decoration: none;">Bibtex</a>]\n' \
+            + ' <br> ' + '\n</html>\n'
+            # +'<div class="bibtex" style="box-sizing: border-box; font-weight: normal; text-decoration: none; display: inline; margin-right: 5px;">'\
+            # +'</dd>\n' +'<p></p>\n' +'</body>\n' +  '<span style="font-weight: normal">' + '@'+ bib_database.entries[j]['ENTRYTYPE']+'{'+bib_database.entries[j]['ID']+',\n'\
+            # + '<br> author    = {'+paper_author+'},\n'\
+            # +' <br> title    = {'+bib_database.entries[j]['title']+'},\n'\
+            # +' <br> booktitle    = {'+paper_booktitle+'},\n'\
+            # +' <br> address    = {'+paper_address+'},\n'\
+            # +' <br> pages    = {'+paper_page+'},\n'\
+            # +' <br> days    = {'+paper_days+'},\n'\
+            # +' <br> month    = {'+paper_month+'},\n'\
+            # +' <br> year    = {'+paper_year+'},\n'\
+            # +' <br> keywords    = {'+paper_keywords+'},\n'\
+            # +' <br> abstract    = {'+paper_abstract+'}\n'\
+            #+' <br> }' + '\n</html>\n'
 
             # writing the code into the file
             f.write(html_template)
-
             # close the file
             f.close()
+
+            #Add to the list of papers
+            linksToPapers.append('<p> <a href = "'+year+'/'+str(Paper_Number[i])+'.html"'+\
+                ' target = "_self" style="text-decoration: none">'+ str(Title[i])+ ' </a> ')
+            linksToPapers.append('<br> '+str(Authors[i])+' </p>')
         # except Exception as e: print(e)
     # else:
     #     print('There is no Registration Record for', Paper_Number[i], '\t', i)
 
 
+#save the index file
+papers_header=r'<!DOCTYPE html> <html> <head>'+\
+    '<meta http-equiv="content-type" content="text/html; charset=UTF-8">'+\
+    '<title>List of papers</title>'+\
+    '<style> '+\
+    'a {font-size: 20px} '+\
+    'a:link {text-decoration:none;color: #000000;} '+\
+	'a:visited {text-decoration:none;color: #000000;} '+\
+	'a:hover {font-weight: bold;color: #000000;} '+\
+	'a:active {text-decoration:underline;font-weight: bold;color: #000000;} '+\
+    '</style> '+\
+    '</head> <body> <p><br></p>'
+
+papers_footer=r'<p><br></p></body></html>'
+f = open(path_current_dir+'/wwwroot/papers.html', 'w', encoding="utf-8")
+f.write(papers_header+'\n')
+for i in range(len(linksToPapers)):
+    f.write(str(linksToPapers[i]+'\n'))
+f.write(papers_footer)
+f.close()
+
 ## Reading all HTML Files in the Directory
-HTML_files=[]
-for file in os.listdir(r'wwwroot/'+year):
-    if file.endswith(".html"):
-        HTML_files.append(r'/'+year+r'/'+file)
+# HTML_files=[]
+# for file in os.listdir(r'wwwroot/'+year):
+#     if file.endswith(".html"):
+#         HTML_files.append(file)
 
 # Open HTML Files
-for i in range(len(HTML_files)):
-    webbrowser.open(HTML_files[i])
+# for i in range(len(HTML_files)):
+#     webbrowser.open("file://"+path_current_dir+"\\wwwroot\\"+year+"\\"+HTML_files[i])
 
-# print('\n\nFinish!')
+print('\n\nFinish!')
